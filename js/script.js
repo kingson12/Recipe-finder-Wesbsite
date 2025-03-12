86998dd8f00a298b40c73a8106d9ae1d225036cb
kingson12 <ngalakingson@gmail.com>
2025-03-12 15:47:00 -0700

    changes

--- js/script.js
+++ js/script.js
@@ -113,19 +113,29 @@ navtoggle.addEventListener("change", () => {
 document.addEventListener("DOMContentLoaded", () => {
   const counters = document.querySelectorAll(".counter");
 
-  counters.forEach((counter) => {
-    const updateCount = () => {
-      const target = +counter.getAttribute("data-target");
-      const count = +counter.innerText;
-      const increment = target / 100;
+  function runCounter() {
+    counters.forEach((counter) => {
+      counter.innerText = "0"; // Reset counter to 0
 
-      if (count < target) {
-        counter.innerText = Math.ceil(count + increment);
-        setTimeout(updateCount, 20);
-      } else {
-        counter.innerText = target;
-      }
-    };
-    updateCount();
-  });
+      const updateCount = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+        const increment = target / 100;
+
+        if (count < target) {
+          counter.innerText = Math.ceil(count + increment);
+          setTimeout(updateCount, 20);
+        } else {
+          counter.innerText = target;
+        }
+      };
+      updateCount();
+    });
+  }
+
+  // Run the counter initially
+  runCounter();
+
+  // Repeat the counter animation every 3 minutes (180,000 milliseconds)
+  setInterval(runCounter, 10000);
 });
